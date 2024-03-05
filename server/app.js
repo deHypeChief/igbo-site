8a71fd3ae39238bfc285a86a561e5ee0b3d67321
deHypeChief <dev.hype7@gmail.com>
2024-03-05 03:09:48 +0100

    ui

--- server/app.js
+++ server/app.js
@@ -16,14 +16,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true'); 
-  next();
-});
-
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
 
