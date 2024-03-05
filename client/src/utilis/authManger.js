55d2f8b57d297f266de0eeb0130948dc99d1b08b
deHypeChief <dev.hype7@gmail.com>
2024-03-05 09:03:43 +0100

    updata

--- client/src/utilis/authManger.js
+++ client/src/utilis/authManger.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = '/api'; // Replace this with your API base URL
+const API_BASE_URL = 'https://igbo-site.vercel.app/api'; // Replace this with your API base URL
 
 const api = axios.create({
     baseURL: API_BASE_URL,
