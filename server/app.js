ba3d579c820f80820fecb2dc8072dbadf92945cc
deHypeChief <dev.hype7@gmail.com>
2024-03-09 20:53:56 +0100

    u

--- server/app.js
+++ server/app.js
@@ -11,38 +11,12 @@ connectToMongoDB();
 
 const app = express();
 const PORT = process.env.PORT || 3600;
-const isProduction = process.env.NODE_ENV === 'production';
 
-// Conditionally enable CORS based on environment
-if (isProduction) {
-  // Allow only main domain in production
-  app.use(cors({ origin: 'https://igbo-site.vercel.app/' }));
-  app.options('*', cors({ origin: 'https://igbo-site.vercel.app/' }));
-
-} else {
-  // Allow localhost in development
-  app.use(cors({ origin: 'http://localhost:5173' }));
-  app.options('*', cors({ origin: 'http://localhost:5173' }));
-}
-
-// Handle preflight requests
-
-// Dynamic CORS configuration based on the incoming request
-app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:5173', 'https://igbo-site.vercel.app/'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Set Access-Control-Allow-Credentials header
-  }
-  // Handle other CORS headers and methods as needed
-  next();
-});
-
+app.use(cors({
+  origin: 'https://igbo-learning.vercel.app',
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-
 
 
 // routes
