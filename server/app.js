5773cbc5f42b02da1b9fca896de0817cf4a5aaf2
deHypeChief <dev.hype7@gmail.com>
2024-03-27 16:56:56 +0100

    done

--- server/app.js
+++ server/app.js
@@ -19,9 +19,9 @@ const app = express();
 const PORT = process.env.PORT || 3600;
 
 // https://learnigboonline.com
-
+// https://igbo-learning.vercel.app
 app.use(cors({
-  origin: process.env.NODE_ENV == "dev" ? 'http://localhost:5173' : 'https://igbo-learning.vercel.app',
+  origin: process.env.NODE_ENV == "dev" ? 'http://localhost:5173' : 'https://learnigboonline.com',
 }));
 
 app.use(express.urlencoded({ extended: false }));
