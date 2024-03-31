088b15b5d33b5902520ca3e25df1e2ad0c9ee5ef
deHypeChief <dev.hype7@gmail.com>
2024-03-31 21:41:28 +0100

    b

--- client/src/pages/student/lesson.jsx
+++ client/src/pages/student/lesson.jsx
@@ -37,7 +37,7 @@ export default function Lesson() {
 
             postUserData('/lesson/oneLesson', { level: id }).then((data) => {
                 eval('obj = ' + data.data.data.note)
-                setNoteData(SplitLesson(obj, 9))
+                setNoteData(SplitLesson(obj, 8))
                 setLessonData(data.data.data)
                 document.getElementById("dasboardTitle").innerText = data.data.data.title 
             }).catch(error => {
